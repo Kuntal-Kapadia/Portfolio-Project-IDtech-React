37b79d65c590d08c6fe37a3c86ba004835103be2
Kuntal <kuntalkapadia1@gmail.com>
2020-07-28 20:02:04 -0700

    Added Logo to footer

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -19,7 +19,7 @@ function Footer(props) {
             </div> 
             <div className ="container-fluid">                             
                 <div className="row align-items-center justify-contents-center"> 
-                <div className="col-4 col-sm-2 offset-1">
+                <div className="col col-sm-2 offset-1">
                         <ul className="list-unstyled">
                             <li><Link to='/home'>Home</Link></li>
                             <li><Link to='/whyID'>Why ID</Link></li>
@@ -27,12 +27,15 @@ function Footer(props) {
                             <li><Link to='/courses'>Courses</Link></li>
                         </ul>
                     </div>                    
-                    <div className="col-6">
+                    <div className="col-6 justify-contents-end align-items-end">
                         <a className="btn btn-social-icon btn-instagram" href="https://www.instagram.com/idtech/"><i className="fa fa-instagram"></i></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com/computercamps"><i className="fa fa-facebook-square"></i></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="https://www.twitter.com/idtechcamps"><i className="fa fa-twitter"></i></a>{' '}
                         <a className="btn btn-social-icon btn-google" href="https://www.youtube.com/channel/UCxfgxJx4I7krS0mykkN-A9Q"><i className="fa fa-youtube"></i></a>{' '}
                         <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/company/the-confident-communicator/"><i className="fa fa-linkedin-square"></i></a>
+                    </div>
+                    <div className="col-3">
+                        <Link to='/home'><img src="/assets/images/logo-id.png" alt="ID"/></Link>
                     </div>
                 </div>
                 
